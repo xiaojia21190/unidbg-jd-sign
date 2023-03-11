cc7e82d2555845ab8699fca2bdac72b2f53467b3
lpp <350331045@qq.com>
2023-03-11 15:45:12 +0800

    init

--- src/main/java/com/spider/unidbgserver/controller/JdController.java
+++ src/main/java/com/spider/unidbgserver/controller/JdController.java
@@ -1,20 +1,18 @@
 package com.spider.unidbgserver.controller;
 
-import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONException;
 import com.spider.unidbgserver.service.JdService;
 import com.spider.unidbgserver.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/jd")
-public class JdController{
+public class JdController {
 
     @Autowired
     JdService jdService;
@@ -35,5 +33,4 @@ public class JdController{
         String res = "&clientVersion=11.0.2&build=97565&client=android&partner=yyds3&sdkVersion=30&lang=zh_CN&harmonyOs=0&networkType=wifi&oaid=" + uuid + "&uuid=" + uuid + "&" + sign;
         return R.success("成功！", res);
     }
-
 }
